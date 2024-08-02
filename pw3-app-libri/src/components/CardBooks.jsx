ee72ccf3e500ed3b86ee8d0eaa59f50dd8a02b9a
gabHenryA <gabrielhenry529@gmail.com>
2024-08-01 22:21:46 -0300

    Limpeza dos arquiivos CSS e criação do primeiro componente

---
+++ pw3-app-libri/src/components/CardBooks.jsx
@@ -0,0 +1,11 @@
+const CardBooks = ({titulo,autor}) => {
+    
+    return(
+        <div>
+            <h3>{titulo}</h3>
+            <p>{autor}</p>
+        </div>
+    )
+}
+
+export default CardBooks
